c36f76d6ffae48fa67a698a9285e06a328d1f9a0
thats-sarthak <sarthakmbd81@gmail.com>
2024-01-09 22:14:00 +0530

    changes

--- ContainsDuplicate2.cpp
+++ ContainsDuplicate2.cpp
@@ -1,15 +1,16 @@
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        for(int i=0; i<nums.size(); i++) {
-            if(mp.count(nums[i]) > 0 && abs(mp[nums[i]]-i) <=k ) {
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.count(nums[i]) > 0 && std::abs(mp[nums[i]] - i) <= k) {
                 return true;
             }
             mp[nums[i]] = i;
         }
         return false;
-        
     }
 };
