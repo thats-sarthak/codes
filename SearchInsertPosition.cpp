76475086d134b82e87f00d3968b53764581be0fa
thats-sarthak <sarthakmbd81@gmail.com>
2024-01-09 22:11:01 +0530

    commit

--- SearchInsertPosition.cpp
+++ SearchInsertPosition.cpp
@@ -1,24 +1,22 @@
+#include <vector>
+
 class Solution {
 public:
 
-    int binarySearch(vector<int>&nums,int start,int end,int target){
-        while(start<=end){
-            int mid = start + (end-start)/2;
+    int binarySearch(std::vector<int>& nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-            if(nums[mid]<target){
-                start = mid+1;
-            }
-            else if(nums[mid]>=target){
-                end = mid-1;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] >= target) {
+                end = mid - 1;
             }
         }
         return start;
     }
 
-
-    int searchInsert(vector<int>& nums, int target) {
-        
-        return binarySearch(nums,0,nums.size()-1,target);
-
+    int searchInsert(std::vector<int>& nums, int target) {
+        return binarySearch(nums, 0, nums.size() - 1, target);
     }
 };
